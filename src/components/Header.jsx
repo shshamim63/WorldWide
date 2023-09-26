a6bd8237b3141f84c1f79e1205a82434b499615b
shshamim63 <shshamim63@gmail.com>
2023-09-26 20:35:05 +0600

    style(header): added header section and some custom design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import {
+  Button,
+  Container,
+  Nav,
+  NavLink,
+  Navbar,
+  NavbarBrand,
+} from "react-bootstrap";
+
 const Header = () => {
-  return <div>Navbar</div>;
+  const [activeHeader, setActiveHeader] = useState("");
+
+  function handleOnActive(headerValue) {
+    setActiveHeader(headerValue);
+  }
+  return (
+    <Navbar>
+      <Container fluid className="pl-5 pr-5">
+        <NavbarBrand>
+          <img src={logo} alt="WorldWise" className="brand-image" />
+        </NavbarBrand>
+        <Nav>
+          <NavLink
+            className={
+              "nav-item" + (activeHeader == "product" ? " active" : "")
+            }
+            onClick={() => handleOnActive("product")}
+          >
+            PRODUCT
+          </NavLink>
+          <NavLink
+            className={
+              "nav-item" + (activeHeader == "pricing" ? " active" : "")
+            }
+            onClick={() => handleOnActive("pricing")}
+          >
+            PRICING
+          </NavLink>
+          <NavLink>
+            <Button>LOG IN</Button>
+          </NavLink>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
