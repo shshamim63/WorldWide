ea60d1a0244f5d6da862cd55e1d61046d1317f42
shshamim63 <shshamim63@gmail.com>
2023-10-09 23:11:04 +0600

    fix(map): changes map styling to view the map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,22 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import "leaflet/dist/leaflet.css";
+
+import { useNavigate } from "react-router-dom";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 const Map = () => {
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
-  
+
   return (
     <div className="map-container" onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        position: {lat} , {lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 23, lng: 50 })}>
-        Change position
-      </button>
+      <MapContainer center={[51.505, -0.09]} zoom={13} className="map">
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={[51.505, -0.09]}>
+          {/* You can add custom content to your markers here */}
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
