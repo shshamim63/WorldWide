c7cb2e94bdc508e386b63ae3bda864cb959025c8
shshamim63 <shshamim63@gmail.com>
2023-10-18 21:16:57 +0600

    feat(not-found): added not found page context

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,26 @@
 import PropTypes from "prop-types";
+import { Stack } from "react-bootstrap";
+
+import notFoundImage from "../assets/notfound.jpg";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = ({ title }) => {
-  return <div>Not Found {title}</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="page-container position-relative" id="not-found-page">
+      <Stack gap={3}>
+        <img src={notFoundImage} alt={title} className="not-foun-image" />
+        <p className="apology-message">{`We're Sorry the page you requested could not be found`}</p>
+        <button
+          className="back-to-home-btn"
+          onClick={() => navigate("/", { replace: true })}
+        >
+          Go to Homepage
+        </button>
+      </Stack>
+    </div>
+  );
 };
 
 NotFound.prototype = {
--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,40 +1,45 @@
+import { Suspense, lazy } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import HomePage from "./HomePage";
-import Product from "./Product";
-import Pricing from "./Pricing";
-import NotFound from "./NotFound";
-import AppLayout from "./AppLayout";
 import CityList from "../components/CityList";
 import CountryList from "../components/CountryList";
 import City from "../components/City";
 import CityForm from "../components/CityForm";
-import Login from "./Login";
 import ProtectedRoute from "./ProtectedRoute";
+import { Spinner } from "react-bootstrap";
+
+const HomePage = lazy(() => import("./HomePage"));
+const Product = lazy(() => import("./Product"));
+const Pricing = lazy(() => import("./Pricing"));
+const Login = lazy(() => import("./Login"));
+const AppLayout = lazy(() => import("./AppLayout"));
+const NotFound = lazy(() => import("./NotFound"));
 
 const Router = () => {
   return (
-    <Routes>
-      <Route index element={<HomePage title="home" />} />
-      <Route path="login" element={<Login title="login" />} />
-      <Route path="product" element={<Product title="product" />} />
-      <Route path="pricing" element={<Pricing title="pricing" />} />
-      <Route
-        path="app"
-        element={
-          <ProtectedRoute>
-            <AppLayout title="app" />
-          </ProtectedRoute>
-        }
-      >
-        <Route index element={<Navigate to="cities" />} />
-        <Route path="countries" element={<CountryList />} />
-        <Route path="cities" element={<CityList />} />
-        <Route path="cities/:id" element={<City />} />
-        <Route path="form" element={<CityForm />} />
-      </Route>
-      <Route path="*" element={<NotFound title="invalid" />} />
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route index element={<HomePage title="home" />} />
+        <Route path="login" element={<Login title="login" />} />
+        <Route path="product" element={<Product title="product" />} />
+        <Route path="pricing" element={<Pricing title="pricing" />} />
+        <Route
+          path="app"
+          element={
+            <ProtectedRoute>
+              <AppLayout title="app" />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Navigate to="cities" />} />
+          <Route path="countries" element={<CountryList />} />
+          <Route path="cities" element={<CityList />} />
+          <Route path="cities/:id" element={<City />} />
+          <Route path="form" element={<CityForm />} />
+        </Route>
+        <Route path="*" element={<NotFound title="Page Not found" />} />
+      </Routes>
+    </Suspense>
   );
 };
 
